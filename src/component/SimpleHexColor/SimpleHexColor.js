96f25e9d0fc14a46cd4ee7bb005a009b48c65ce7
sneha2395 <agarwal.sneha.231995@gmail.com>
2022-10-30 17:10:13 +0530

    Hex Color is generating

--- src/component/SimpleHexColor/SimpleHexColor.js
+++ src/component/SimpleHexColor/SimpleHexColor.js
@@ -1,50 +1,69 @@
 import React, { useEffect, useState } from "react";
 import './simpleHex.css';
-const hexs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-// const colors=["red", "green", "blue", "yellow", "pink", "black", "brown", "purple", "rgba(133,122,200", "rgba(155,344),567)"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+let colors = [];
 
-let hexColor = "#";
-  
-  for (let i = 0; i < 6; i++) {
-    console.log(hexColor, i);
-    hexColor += hexs[getRandomNumber()];
-    console.log(hexColor, i);
-  }
 
-  
-  function getRandomNumber() {
-    return Math.floor(Math.random() * hexs.length);
+/*this function returns a random number between 0 to 16 */
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * hex.length);
+};
+
+/*this function returns hex code color in string*/
+const generateRandomColor = () => {
+  let hexColor = "#";
+1  ;for ( let i = 0; i < 6; i++) {
+    const randomHexArrayIndex = generateRandomNumber();
+    hexColor = hexColor + hex[randomHexArrayIndex];
   }
+  return hexColor;
+};
+ 
+/* this function returns array containing 10 hex colors
+*/
+const generatePallete = () => {
+   const colorPallete = [];
+   
+
+   for( let i = 0; i < 10; i++){
+    let color = generateRandomColor();
+    // console.log(generateRandomColor());
+    colorPallete.push(color)
+   } 
+   console.log(colorPallete);
+   return colorPallete;
+   
+}
+
+
 
 const SimpleHexColor = () => {
-  const[toggle, setToggle]= React.useState(false);
+  const [toggle, setToggle] = React.useState(false);
 
-  const[count, setCount] = useState(0);
   useEffect(() => {
 
-       document.title = `you clicked ${count} times`;
-      //  Math.floor(Math.random() *colors.length)
-  });
+    const randomColors = generatePallete();
+     colors = randomColors;
 
-  
+  });
 
   return (
     <div className="main_container">
-      
-        < button type="button" className="simple-button" onClick={() => setToggle(true)}
-       >Generate hex color</button>
-        {/* <button onClick={() => setCount(count + 1)}></button> */}
-      {toggle &&(
-      <div className="simpleHexContainer">
-        <div className="stripe_simple_hex">
-          {hexs.map((color, index) => (
-            <div className="stripe_color_box" style={{ width: '150px', height: '50px', color: "red", "background-color": color }}>
-              {/* <p>{{ index }}</p> */}
-            </div>
-          ))}
+
+      < button type="button" className="simple-button" onClick={() => setToggle(!toggle)}
+      >Generate hex color</button>
+      {/* <button onClick={() => setCount(count + 1)}></button> */}
+      {toggle && (
+        <div className="simpleHexContainer">
+          <div className="stripe_simple_hex">
+            {colors.map((color, index) => (
+              <div className="stripe_color_box" style={{ width: '150px', height: '50px', color: "red", "background-color": color }}>
+
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-     ) }
+      )}
     </div>
   );
 }
